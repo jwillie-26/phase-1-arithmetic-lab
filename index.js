e4bf860980473f071a8e65a67e377e33fed159ab
jwillie-26 <105637783+jwillie-26@users.noreply.github.com>
2022-11-21 14:58:11 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
+//Create a variable called multiply set to an equation that will multiply the variables num1 and num2; the result of the multiplication should be 62
+a = int(input("2:")) 
+b = int(input("31: "))  
+product = a*b
 
-// Write your code here
+
+
+ 
+
---
+++ test.js
@@ -0,0 +1,5 @@
+//Create a variable called max that finds the highest number in a set; the value returned should be 20
+function max(20,10){
+    if(20>10){
+      return 20;}
+    }
