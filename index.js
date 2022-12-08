c37981c33531f87d75a02d5f49d1c7b52f3f8557
jwillie-26 <105637783+jwillie-26@users.noreply.github.com>
2022-12-08 10:29:25 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 //Create a variable called multiply set to an equation that will multiply the variables num1 and num2; the result of the multiplication should be 62
-a = int(input("2:")) 
-b = int(input("31: "))  
-product = a*b
+const num1 = 31;
+const num2 =  2;
+const num3 = 24;
+const num4 = 5;
+let multiply = num1 * num2;
+let random = Math.floor(Math.random() * 10) + 1;
+let mod = num3 % num4;
+let max =Math.max(1, 6, 8, 20)
 
 
 
